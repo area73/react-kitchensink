6b00893a7cecb01a6705032d65525fbd8858f16e
rerades <rerades@siete3.com>
2019-03-14 09:03:24 +0100

    change starting page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.scss';
-// import App from './App';
+import App from './App';
 
-import GithubUserSearch from './githubUserSearch/GithubUserSearch.js';
+
 
 import * as serviceWorker from './serviceWorker';
 
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<GithubUserSearch />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
